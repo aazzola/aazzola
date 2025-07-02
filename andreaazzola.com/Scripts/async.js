c8878091b2b11eb229716cc7aa2afac2504ded63
Andrea <andrea.azzola@gmail.com>
2025-07-02 20:04:03 +0200

    Initial deploy of static site

---
+++ andreaazzola.com/Scripts/async.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    // Analytics
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','../www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-7478963-1', 'auto');
+    ga('send', 'pageview');
+
+    // Google+
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = '../apis.google.com/js/plusone3083.js?onload=onLoadCallback';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
